3bef787b3361ca32f9f0016edd164ce5ec858a58
CollinEdward <manorengaard@icloud.com>
2021-11-15 19:50:20 +0100

    Interacting with functions, initial commit

---
+++ Functions with tkinter/Interact-with-functions.py
@@ -0,0 +1,13 @@
+
+ # Creating the base GUI, so you can get a picture of how little lines it takes, with a simple and easy moduel.
+
+from tkinter import * # Here we import everything (*) from tkinter
+
+
+ # Defining the GUI window. This is a veriable and can be named anything you like
+root = Tk()
+
+ # The mainloop is so the window keep running until you press on x or exit the script from terminal
+root.mainloop() 
+
+
